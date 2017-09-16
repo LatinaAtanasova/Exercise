72998af4221b0e6420423fcfb74bc6ea8ff1ca7b
LatinaAtanasova <latina_atanasova@abv.bg>
2017-09-16 23:54:14 +0300

    adding new file

---
+++ Test numbers/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var m = int.Parse(Console.ReadLine());
+            var sumBoundary = int.Parse(Console.ReadLine());
+
+            var counter = 0;
+            var totalSum = 0;
+            bool sumReached = false;
+
+            for (int i = n; i >= 1; i--)
+            {
+                for (int j = 1; j <= m; j++)
+                {
+                    var result = 3 * (i * j);
+                    totalSum = totalSum + result;
+                    counter = counter + 1;
+                    if (totalSum >= sumBoundary)
+                    {
+                        sumReached = true;
+                        goto Finish;
+                    }
+                }
+            }
+        Finish: if (sumReached == false)
+            {
+                Console.WriteLine($"{counter} combinations");
+                Console.WriteLine($"Sum: {totalSum}");
+            }
+            else
+            {
+                Console.WriteLine($"{counter} combinations");
+                Console.WriteLine($"Sum: {totalSum} >= {sumBoundary}");
+            }
+            
+        }
+    }
+}
